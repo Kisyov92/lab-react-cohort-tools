c4e64cb3841361f403cb36716bc0ca1134d6f06b
Atanas Kisyov <kisyov92@gmail.com>
2024-08-08 13:07:39 +0200

    Completed the lab

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./App";
+// import App from "./App";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import StudentDetailsPage from "./pages/StudentDetailsPage";
+import UserProfilePage from "./pages/UserProfilePage";
+import Navbar from "./components/Navbar";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/students/:studentId" element={<StudentDetailsPage />} />
+        <Route path="/profile" element={<UserProfilePage />} />
+      </Routes>
+    </Router>
   </React.StrictMode>
 );
